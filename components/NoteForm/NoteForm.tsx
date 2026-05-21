3b1e697faf05fde6848bda13f7d3541dd805d820
d-levchenko <dl.qubb@gmail.com>
2026-05-21 20:47:19 +0300

    updated form validation; changed description meta tag

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -26,9 +26,7 @@ const validation = Yup.object().shape({
     .min(3, 'Title must be at least 3 characters')
     .max(50, 'Title must be at most 50 characters')
     .required('Title is required'),
-  content: Yup.string()
-    .max(500, 'Content must be at most 500 characters')
-    .required('Content is required'),
+  content: Yup.string().max(500, 'Content must be at most 500 characters'),
   tag: Yup.string()
     .oneOf(
       ['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'],
